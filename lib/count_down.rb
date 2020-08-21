20f8794737750f28dbba76dc314b501a0a0b00fa
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-08-21 20:47:05 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,6 +1,6 @@
-magic_exit_number = 7
-count = 0
-while count < 10 && count != magic_exit_number do
-  puts "I am the #{count}, I love to count!" # Work
-  count = count + 1
+number = 10
+while number > 0
+  puts number
+  number -= 1
+  puts "Happy New Year!"
 end
